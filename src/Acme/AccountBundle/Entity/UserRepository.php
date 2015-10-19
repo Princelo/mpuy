1c3d18a201849f3916e62b574b6a970dff1698c7
Princelo <lamkimcheung@gmail.com>
2015-10-19 17:11:19 +0800

    order

--- src/Acme/AccountBundle/Entity/UserRepository.php
+++ src/Acme/AccountBundle/Entity/UserRepository.php
@@ -80,7 +80,7 @@ class UserRepository extends EntityRepository implements UserProviderInterface
     {
         return $this->getEntityManager()
             ->createQueryBuilder()
-            ->select('p')
+            ->select('u')
             ->from('AcmeAccountBundle:User', 'u')
             ->innerJoin('u.fansUsers', 'fu')
             ->where('u.id = :user_id' )
@@ -95,7 +95,7 @@ class UserRepository extends EntityRepository implements UserProviderInterface
     {
         return $this->getEntityManager()
             ->createQueryBuilder()
-            ->select('p')
+            ->select('u')
             ->from('AcmeAccountBundle:User', 'u')
             ->innerJoin('u.followedUsers', 'fu')
             ->where('u.id = :user_id' )
