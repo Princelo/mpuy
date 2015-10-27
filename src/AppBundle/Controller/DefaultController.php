e40d02173f23dbd30861680c432c70c383ce1b11
Princelo <lamkimcheung@gmail.com>
2015-10-27 10:39:09 +0800

    little

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -21,6 +21,9 @@ class DefaultController extends Controller implements WechatTokenGetterInterface
         $nickname = $user->getNickName();
         $em = $this->getDoctrine()->getEntityManager();
         $products = $em->getRepository('AppBundle:Product')->getHomeProducts($user, 0, Constants::PRODUCT_PER_PAGE);
+        $payment = $em->getRepository('AppBundle:Payment')->getHighestPayment($products[0]);
+        $bidList = $em->getRepository('AppBundle:Payment')->getBidList($products[0]);
+        return new JsonResponse($bidList);
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
             'avatar' => $avatar,
