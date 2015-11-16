43f193568adf9ec66e1e0663d11f9de7e952fc6d
Princelo <lamkimcheung@gmail.com>
2015-11-16 16:08:23 +0800

    commands

--- src/AppBundle/Command/OrderCreateCommand.php
+++ src/AppBundle/Command/OrderCreateCommand.php
@@ -9,6 +9,7 @@ namespace AppBundle\Command;
 
 use AppBundle\Entity\AuctionOrder;
 use AppBundle\Entity\Constants;
+use AppBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -31,16 +32,19 @@ class OrderCreateCommand extends ContainerAwareCommand
         $now = new \DateTime('now');
         $products = $em->getRepository('AppBundle:Product')->getProductsExpired($now);
         foreach($products as $i => $p) {
-            $order = new AuctionOrder();
-            $order->setProduct($p);
-            $order->setPayment($p->getTopPayment());
-            $order->setBuyer($p->getTopPayment()->getPayUser());
-            $order->setSeller($p->getUser());
-            $order->setIsDelete(false);
-            $order->setOrderSn($now->format('YmdHis').sprintf("%04s", $i));
-            $order->setStatus(Constants::ORDER_CREATE);
-            $em->persist($order);
-            $em->flush();
+            if ($p->getTopPayment() === null) {
+            } else {
+                $order = new AuctionOrder();
+                $order->setProduct($p);
+                $order->setPayment($p->getTopPayment());
+                $order->setBuyer($p->getTopPayment()->getPayUser());
+                $order->setSeller($p->getUser());
+                $order->setIsDelete(false);
+                $order->setOrderSn($now->format('YmdHis').sprintf("%04s", $i));
+                $order->setStatus(Constants::ORDER_CREATE);
+                $em->persist($order);
+                $em->flush();
+            }
         }
     }
 }
