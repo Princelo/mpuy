5a4d63e770326a3e71a1e22d84cd443ddf92c626
Princelo <lamkimcheung@gmail.com>
2015-11-23 17:07:06 +0800

    little

--- src/AppBundle/DependencyInjection/Compiler/DoctrineEntityListenerPass.php
+++ src/AppBundle/DependencyInjection/Compiler/DoctrineEntityListenerPass.php
@@ -12,7 +12,7 @@ class DoctrineEntityListenerPass implements CompilerPassInterface
         $services = $container->findTaggedServiceIds('doctrine.entity_listener');
         echo "<pre>";
         print_r($definition);
-        print_r($services);
+        print_r($services);exit;
 
         foreach ($services as $service => $attributes) {
             $definition->addMethodCall(
