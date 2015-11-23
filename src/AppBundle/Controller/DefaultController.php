4d462b455d6b7a90d4aa8f412db6ce8546c444e1
Princelo <lamkimcheung@gmail.com>
2015-11-23 17:20:20 +0800

    little

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Constants;
-use Doctrine\DBAL\Events;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -16,16 +15,11 @@ class DefaultController extends Controller implements WechatTokenGetterInterface
      */
     public function indexAction(Request $request)
     {
-        $test = new TestEvent();
-        $em = $this->getDoctrine()->getEntityManager();
-        $metadataFactory = $em->getMetadataFactory();
-        $evm = $em->getEventManager();
-        $evm->addEventListener(\Doctrine\ORM\Events::loadClassMetadata, $test);
-
         // replace this example code with whatever you need
         $user = $this->getUser();
         $avatar = $user->getAvatar();
         $nickname = $user->getNickName();
+        $em = $this->getDoctrine()->getEntityManager();
         $products = $em->getRepository('AppBundle:Product')->getHomeProducts($user, 0, Constants::PRODUCT_PER_PAGE);
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
@@ -33,19 +27,5 @@ class DefaultController extends Controller implements WechatTokenGetterInterface
             'nickname' => $nickname,
             'products' => $products,
         ));
-    }
-}
-
-class TestEvent
-{
-    public function loadClassMetadata(\Doctrine\ORM\Event\LoadClassMetadataEventArgs $eventArgs)
-    {
-        $classMetadata = $eventArgs->getClassMetadata();
-        $fieldMapping = array(
-            'fieldName' => 'about',
-            'type' => 'string',
-            'length' => 255
-        );
-        $classMetadata->mapField($fieldMapping);
     }
 }
