0da7415355945f664bf8e48549c758762c02baba
Princelo <lamkimcheung@gmail.com>
2015-12-07 04:29:41 +0800

    little

--- src/Acme/AdminBundle/Controller/SecuredController.php
+++ src/Acme/AdminBundle/Controller/SecuredController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\BackendBundle\Controller;
+namespace Acme\AdminBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
